370f3eb690aedb82d440e2adb12ba38d0d1aadea
Chadwell Clark <wcclark23@comcast.net>
2021-06-16 13:21:52 -0500

    Post add Completed minus blog functionality

--- TabloidCLI/Repositories/PostRepository.cs
+++ TabloidCLI/Repositories/PostRepository.cs
@@ -5,7 +5,7 @@ using TabloidCLI.Models;
 
 namespace TabloidCLI.Repositories
 {
-    public class PostRepository : DatabaseConnector//, IRepository<Post>
+    public class PostRepository : DatabaseConnector, IRepository<Post>
     {
         public PostRepository(string connectionString) : base(connectionString) { }
 
@@ -79,7 +79,22 @@ namespace TabloidCLI.Repositories
 
         public void Insert(Post post)
         {
-            throw new NotImplementedException();
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"INSERT INTO Post (Title, URL, PublishDateTime, AuthorId, BlogId)
+                                             VALUES (@title, @url, @publishDateTime, @authorId, @blogId)";
+                    cmd.Parameters.AddWithValue("@title", post.Title);
+                    cmd.Parameters.AddWithValue("@url", post.Url);
+                    cmd.Parameters.AddWithValue("@publishDateTime", post.PublishDateTime);
+                    cmd.Parameters.AddWithValue("@authorId", post.Author.Id);
+                    cmd.Parameters.AddWithValue("@blogId", 6); //);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
         }
 
         public void Update(Post post)
