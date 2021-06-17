7da78140ae96bbf9d15851296d8434cced8196a6
Micaela Colpi <micaelacolpi@gmail.com>
2021-06-17 13:20:01 -0500

    Add tag for blog

--- TabloidCLI/Repositories/BlogRepository.cs
+++ TabloidCLI/Repositories/BlogRepository.cs
@@ -149,22 +149,52 @@ namespace TabloidCLI
             }
         }
 
-        //public void InsertTag(Author author, Tag tag)
+        //public void InsertTag(Blog blog, Tag tag)
         //{
         //    using (SqlConnection conn = Connection)
         //    {
         //        conn.Open();
         //        using (SqlCommand cmd = conn.CreateCommand())
         //        {
-        //            cmd.CommandText = @"INSERT INTO AuthorTag (AuthorId, TagId)
-        //                                               VALUES (@authorId, @tagId)";
-        //            cmd.Parameters.AddWithValue("@authorId", author.Id);
+        //            cmd.CommandText = @"INSERT INTO BlogTag (BlogId, TagId)
+        //                                               VALUES (@blogId, @tagId)";
+        //            cmd.Parameters.AddWithValue("@blogId", blog.Id);
         //            cmd.Parameters.AddWithValue("@tagId", tag.Id);
         //            cmd.ExecuteNonQuery();
         //        }
         //    }
         //}
 
+//private void AddTag()
+//        {
+//            Blog blog = _blogRepository.Get(_blogId);
+
+//        Console.WriteLine($"Which tag would you like to add to {blog.Name}?");
+//            List<Tag> tags = _tagRepository.GetAll();
+
+//            for (int i = 0; i<tags.Count; i++)
+//            {
+//                Tag tag = tags[i];
+//        Console.WriteLine($" {i + 1}) {tag.Name}");
+//            }
+//    Console.Write("> ");
+
+//            string input = Console.ReadLine();
+//            try
+//            {
+//                int choice = int.Parse(input);
+//    Tag tag = tags[choice - 1];
+//    _blogRepository.InsertTag(blog, tag);
+//            }
+//            catch (Exception ex)
+//{
+//    Console.WriteLine("Invalid Selection. Won't add any tags.");
+//}
+//        }
+
+       
+        
+        
         //public void DeleteTag(int authorId, int tagId)
         //{
         //    using (SqlConnection conn = Connection)
